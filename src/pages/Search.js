5a0162cbc8bf16bdb56cfdfee25667ee8e180c45
RandomCodingBoy <>
2021-08-06 01:02:14 +0000

    search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/styles.css';
-import Information from './info.js';
+import { daos } from '../data/daos.js';
+import slugify from'../functions/slugify.js';
 
 class Search extends Component {
 
@@ -32,22 +33,34 @@ class Search extends Component {
       marginTop:'5vh',
       marginBottom:'10vh'
     }
-    const items = Information.filter((data)=>{
+    const items = daos.filter((data)=>{
+      input = this.state.search.toLowerCase()
       if(this.state.search == null)
           return data
-      else if(data.name.toLowerCase().includes(this.state.search.toLowerCase()) || data.country.toLowerCase().includes(this.state.search.toLowerCase())){
-          return data
+      else if(element.daoName.includes(input) || element.primaryGoal.includes(input) || element.howFunded.includes(input) || element.howVote.includes(input) || element.howToken(input) || element.techStack(input) || element.otherInterests(input)) {
+        return data
       }
     }).map(data=>{
       return(
       <div>
-        <ul>
-          <li style={{position:'relative',left:'10vh'}}>
-            <span style={styleInfo}>{data.name}</span>
-            <span style={styleInfo}>{data.age}</span>
-            <span style={styleInfo}>{data.country}</span>
-          </li>
-        </ul>
+
+        <li>
+          <div className='cardRank flex'key={index}>
+            <div class='cardLi'>
+              <a style={{"text-decoration":"none","color":"navy"}} href={slugify(element.daoName)}> {element.daoName}</a>
+            </div>
+            <div class='logoGroup'>
+              <a class='logo'href={element.twitterLink}>
+                <img class='logo'src="http://icons.iconarchive.com/icons/ampeross/smooth/512/Twitter-icon.png"/>
+              </a>
+              <a class='logo'href={element.discordLink}>
+                <img class='logo'src="https://i.ibb.co/cv4N5PF/d-icon-1.png"/>
+              </a>
+            </div>
+          </div>
+          &nbsp;
+        </li>
+
       </div>
       )
     })
@@ -55,93 +68,12 @@ class Search extends Component {
     return (
       <div>
       <input type="text" placeholder="Enter item to be searched" style={elementStyle} onChange={(e)=>this.searchSpace(e)} />
-      {items}
+      <ul>
+        {items}
+      </ul>
       </div>
     )
   }
 }
 
 export default Search;
-
-/*import * as React from "react"; 
-import '../styles/custom-classes.css'
-import '../styles/styles.css'
-import useSearch from '../functions/useSearch'
-var { daos } = require('../data/daos.js'); 
-export default function Search() {
-  var data = [  
-   {  
-      "id":"1",
-      "employee_name":"Tiger Nixon",
-      "employee_salary":"320800",
-      "employee_age":"61",
-      "profile_image":"default_profile.png"
-   },
-   {  
-      "id":"2",
-      "employee_name":"Garrett Winters",
-      "employee_salary":"434343",
-      "employee_age":"63",
-      "profile_image":"default_profile.png"
-   },
-   {  
-      "id":"3",
-      "employee_name":"Ashton Cox",
-      "employee_salary":"86000",
-      "employee_age":"66",
-      "profile_image":"default_profile.png"
-   },
-   {  
-      "id":"4",
-      "employee_name":"Cedric Kelly",
-      "employee_salary":"433060",
-      "employee_age":"22",
-      "profile_image":"default_profile.png"
-   }
-];
-  return (
-    <>
-    <div class="container" style={{"padding": `${50}}px ${250}px`}}>
-      <form role="form">
-            <div class="form-group">
-              <input type="input" class="form-control input-lg" id="txt-search" placeholder="Type your search character"/>
-            </div>
-      </form>
-      <h1>hi</h1>
-      <div id="filter-records">
-      </div>
-    </div>
-    {
-      document.querySelector('#txt-search').keyup(function(){
-            var searchField = document.querySelector(this).value;
-			if(searchField === '')  {
-				document.querySelector('#filter-records').html('');
-				return;
-			}
-            var regex = new RegExp(searchField, "i");
-            var output = '<div class="row">';
-            var count = 1;
-			  $.each(data, function(key, val){
-				if ((val.employee_salary.search(regex) != -1) || (val.employee_name.search(regex) != -1)) {
-				  output += '<div class="col-md-6 well">';
-				  output += '<div class="col-md-3"><img class="img-responsive" src="'+val.profile_image+'" alt="'+ val.employee_name +'" /></div>';
-				  output += '<div class="col-md-7">';
-				  output += '<h5>' + val.employee_name + '</h5>';
-				  output += '<p>' + val.employee_salary + '</p>'
-				  output += '</div>';
-				  output += '</div>';
-				  if(count%2 == 0){
-					output += '</div><div class="row">'
-				  }
-				  count++;
-				}
-			  });
-			  output += '</div>';
-			  document.querySelector('#filter-records').html(output);
-        })
-    }
-    </>
-  );
-}
-//primarygoal, website, howMember, howFunded, howVote, howToken, techStack, otherInterests
-*/
